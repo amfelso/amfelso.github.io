d862a4090d9d5f1df4da8e2b3aa0b1277e3e90d0
August Felso <augustfelso@gmail.com>
2023-12-28 23:36:48 -0500

    Make thumbnails dynamic

--- src/components/ProjectGrid.js
+++ src/components/ProjectGrid.js
@@ -14,6 +14,7 @@ const styles = theme => ({
     },
     card: {
         display: 'flex',
+        height: '180px',
         "&:hover": {
             background: theme.palette.grey[100],
             cursor: "pointer"
@@ -32,15 +33,15 @@ const projects = [
         title: 'Web Chat App',
         id: 'e05801f6-ee2d-4994-ba9e-5182c139acf3',
         date: 'Jul 2021',
-        description:
-            'Web chat application using React & Firebase.',
+        description: 'Web chat application using React & Firebase.',
+        image: 'https://firebase.google.com/static/images/brand-guidelines/logo-vertical.png'
     },
     {
         title: 'Cloud ETL Pipeline',
         id: 'fd7bb2ed-6fa7-459d-975e-07cf75cc6342',
         date: 'Dec 2022',
-        description:
-            'An ETL pipeline to process events from worldwide news sources.',
+        description: 'An ETL pipeline to process events from worldwide news sources.',
+        image: 'https://d1.awsstatic.com/logos/aws-logo-lockups/poweredbyaws/PB_AWS_logo_RGB_stacked_REV_SQ.91cd4af40773cbfbd15577a3c2b8a346fe3e8fa2.png'
     },
 ];
 
@@ -69,7 +70,7 @@ function ProjectGrid(props) {
                         <Hidden xsDown>
                             <CardMedia
                                 className={classes.cardMedia}
-                                image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
+                                image={project.image}
                                 title="Image title"
                             />
                         </Hidden>
